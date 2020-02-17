dfe70a0e259f3c344aedb919ebeaef2473e0c4e6
David Clarke <davidclarke@zebracrossing.co.nz>
2020-02-17 13:11:34 +1300

    All tests pass - slow for large N & M

---
+++ solution.cs
@@ -0,0 +1,188 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+class Solution {
+
+    private int _rowCount;
+    private int _colCount;
+    private HashSet<int>[] _countryMap;
+    private int[,] _mapData;
+
+    public int solution(int[,] A) {
+        // N & M in range 1..300,000
+        // number of elements in range 1..300,000
+        // each element is in range -1,000,000,000..1,000,000,000
+        if (A.Length == 1 || A.Cast<int>().All(elt => elt == A[0,0])) return 1;
+
+        int countryCount = 0;
+        _mapData = A;
+        _colCount = _mapData.GetLength(0);
+        _rowCount = _mapData.GetLength(1);
+        _countryMap = new HashSet<int>[_rowCount].Select(elt => new HashSet<int>()).ToArray();
+        for (int y = 0; y < _rowCount; y++)
+        {
+            for (int x = 0; x < _colCount; x++)
+            {
+                if (_countryMap[y].Contains(x)) continue;
+
+                // new country, find all elements in country
+                countryCount++;
+                _countryMap[y].Add(x);
+                SearchEast(x, y, _mapData[x, y]);
+                SearchSouth(x, y, _mapData[x, y]);
+            }
+
+        }
+
+        return countryCount;
+    }
+
+    private void findAllAdjacentNorth(int x, int y, int countryCode)
+    {
+        _countryMap[y].Add(x);
+        SearchWest(x, y, countryCode);
+        SearchNorth(x, y, countryCode);
+        SearchEast(x, y, countryCode);
+    }
+
+    private void findAllAdjacentWest(int x, int y, int countryCode)
+    {
+        _countryMap[y].Add(x);
+        SearchNorth(x, y, countryCode);
+        SearchWest(x, y, countryCode);
+        SearchSouth(x, y, countryCode);
+    }
+
+    private void findAllAdjacentEast(int x, int y, int countryCode)
+    {
+        _countryMap[y].Add(x);
+        SearchNorth(x, y, countryCode);
+        SearchEast(x, y, countryCode);
+        SearchSouth(x, y, countryCode);
+    }
+
+    private void findAllAdjacentSouth(int x, int y, int countryCode)
+    {
+        _countryMap[y].Add(x);
+        SearchEast(x, y, countryCode);
+        SearchSouth(x, y, countryCode);
+        SearchWest(x, y, countryCode);
+    }
+
+    private void SearchSouth(int x, int y, int countryCode)
+    {
+        if (y < _rowCount - 1 && _mapData[x, y + 1] == countryCode && !_countryMap[y + 1].Contains(x)) findAllAdjacentSouth(x, y + 1, countryCode);
+    }
+
+    private void SearchEast(int x, int y, int countryCode)
+    {
+        if (x < _colCount - 1 && _mapData[x + 1, y] == countryCode && !_countryMap[y].Contains(x + 1)) findAllAdjacentEast(x + 1, y, countryCode);
+    }
+
+    private void SearchNorth(int x, int y, int countryCode)
+    {
+        if (y > 0 && _mapData[x, y - 1] == countryCode && !_countryMap[y - 1].Contains(x)) findAllAdjacentNorth(x, y - 1, countryCode);
+    }
+
+    private void SearchWest(int x, int y, int countryCode)
+    {
+        if (x > 0 && _mapData[x - 1, y] == countryCode && !_countryMap[y].Contains(x - 1)) findAllAdjacentWest(x - 1, y, countryCode);
+    }
+
+
+}
+ 
+public class A_codility_solution_should
+{
+
+    public static IEnumerable<object[]> MatrixData
+    {
+        get
+        {
+            return new []
+            {
+                new object[] { new int[,] { { 1, 2 },
+                                            { 2, 2 } }, 2 },
+                new object[] { new int[,] { { 5, 4, 4 },
+                                            { 4, 3, 4 } }, 4 },
+                new object[] { new int[,] { { 0 } }, 1 },
+                new object[] { new int[,] { { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 } }, 1 },
+                new object[] { new int[,] { { 1, 2, 3 } }, 3 },
+                new object[] { new int[,] { { 1 }, { 2 }, { 3 } }, 3 },
+                new object[] { new int[,] { { 1, 2 }, { 3, 4 } }, 4 },
+                new object[] { new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } }, 6 },
+                new object[] { new int[,] { { 1, 2, 3 }, { 4, 5, 6 } }, 6}, 
+
+                new object[] { new int[,] { { 1, 1, 1 } }, 1 },
+                new object[] { new int[,] { { 1 }, { 1 }, { 1 } }, 1 },
+                new object[] { new int[,] { { 1, 1 }, { 1, 1 } }, 1 },
+                new object[] { new int[,] { { 1, 1 }, { 1, 1 }, { 1, 1 } }, 1 },
+                new object[] { new int[,] { { 1, 1, 1 }, { 1, 1, 1 } }, 1}, 
+
+                new object[] { new int[,] { { 1, 2, 1 } }, 3 },
+                new object[] { new int[,] { { 1 }, { 2 }, { 1 } }, 3 },
+                new object[] { new int[,] { { 1, 2 },
+                                            { 1, 2 } }, 2 },
+                new object[] { new int[,] { { 1, 2 }, { 1, 2 }, { 1, 2 } }, 2 },
+                new object[] { new int[,] { { 1, 2, 1 }, { 1, 2, 1 } }, 3}, 
+                
+                new object[] { new int[,] { { 5, 4, 4 }, 
+                                            { 4, 3, 4 },
+                                            { 3, 2, 4 } }, 6 },
+                new object[] { new int[,] { { 5, 4, 4 }, 
+                                            { 4, 3, 4 },
+                                            { 3, 2, 4 },
+                                            { 2, 2, 2 } }, 6 },
+                new object[] { new int[,] { { 5, 4, 4 }, 
+                                            { 4, 3, 4 },
+                                            { 3, 2, 4 },
+                                            { 2, 2, 2 }, 
+                                            { 3, 3, 4 }, 
+                                            { 1, 4, 4 }, 
+                                            { 4, 1, 1 } }, 11 },
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(MatrixData))]
+    public void pass_the_example_test(int[,] testData, int expected)
+    {
+        // arrange
+        var soln = new Solution();
+
+        // act
+        var output = soln.solution(testData);
+
+        // assert
+        Assert.Equal(expected, output);
+    }
+
+    [Fact]
+    public void pass_large_array_test()
+    {
+        // arrange
+        var soln = new Solution();
+        var rand = new Random();
+        var colCount = 3000;
+        var rowCount = 20000;
+        var numbers = Enumerable.Range(0, 45).ToList();
+        var testData = new int[colCount,rowCount];
+        for (int y = 0; y < rowCount; y++)
+        {
+            for (int x = 0; x < colCount; x++)
+            {
+                var idx = rand.Next(0, numbers.Count);
+                testData[x, y] = numbers[idx];
+            }
+        }
+
+        // act
+        var output = soln.solution(testData);
+
+        // assert
+        Assert.True(true);
+    }
+}
